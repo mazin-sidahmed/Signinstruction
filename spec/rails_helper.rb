354cccf404a641781aae794cbea2b52ea981d1cd
Faris Mohammed <farismosman@gmail.com>
2017-06-01 22:42:24 +0200

    added mongodb docker compose file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,11 +23,6 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migration and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-# require database cleaner at the top level
-require 'database_cleaner'
-
 # [...]
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
@@ -56,17 +51,8 @@ RSpec.configuration do |config|
   end
   # [...]
 end
-ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
