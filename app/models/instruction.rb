f47471b561c68b8927b02538a581b1bd93658d3b
mazin-sidahmed <mazinsidahmed@gmail.com>
2017-06-27 19:10:01 +0300

    split the word for results that do not exist

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -6,17 +6,12 @@ class Instruction
   field :sign, type: String
 
   def self.find(words)
-    instructions = Instruction.where(:word.in => words)
     signs = []
-
+    instructions = Instruction.where(:word.in => words).to_a
     words.each do |word|
-      instructions.each do |instruction|
-        if word == instruction.word
-          signs.push(instruction.sign)
-        end
-      end
+      instruction = instructions.find { |instruction| instruction.word == word }
+      instruction ? signs.push(instruction.sign) : signs.concat(word.split(''))
     end
-
     return signs
   end
 end
